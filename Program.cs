174ff26d571c75aa66a602137a4c12e2191c82d5
Emin Yildirim <mhd4201@gmail.com>
2026-01-10 19:54:44 +0300

    Swagger Eklendi

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -9,6 +12,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();  
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
