8674bd60d1fd0b652dc2fd2d943ad8db6247f106
Emin Yildirim <mhd4201@gmail.com>
2026-01-12 18:36:23 +0300

    Sayfalama(Pagination) yapıldı

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -41,8 +41,10 @@ namespace api.Repository
                     stocks = query.IsDecsending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
                 }
             }
+            
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-            return await stocks.ToListAsync();
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
